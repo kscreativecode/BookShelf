05f9a65fadb0d359fd52c0a2852bdeec69448d4f
Kira <k.schlockermann@googlemail.com>
2024-07-22 20:46:21 +0200

    Saving Data

--- bookapp.js
+++ bookapp.js
@@ -1,2 +1,23 @@
-'use strict'
+
+
+    const ul = document.querySelector('ul');
+    const input = document.getElementById("Eingabe");
+
+    let itemsArray = localStorage.getItem('items') ?
+JSON.parse(localStorage.getItem('items')) : [];
+  
+itemsArray.forEach(addBook);
+function addBook(text){
+  const li = document.createElement('li')
+  li.textContent = text;
+  ul.appendChild(li);
+}
+function add(){
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    addBook(input.value);
+    input.value = '';
+  }
+
+
 
