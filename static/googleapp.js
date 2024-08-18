a6fde7bdc6f236ceb5d1b54fd8040190da83e154
Kira <k.schlockermann@googlemail.com>
2024-08-18 20:32:16 +0200

    Sorting

--- static/googleapp.js
+++ static/googleapp.js
@@ -1,231 +1,260 @@
-const apiKey = 'AIzaSyDNmwXFFevatsj1Gr_HOgtGychQCkwWoRc';
+const apiKey = "AIzaSyDNmwXFFevatsj1Gr_HOgtGychQCkwWoRc";
 
 function fetchBooks(query, apiKey) {
-    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`)
-        .then(response => response.json())
-        .then(data => data.items)
-        .catch(error => {
-            console.error('Fehler beim Abrufen der Bücher:', error);
-            return [];
-        });
+  return fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => data.items)
+    .catch((error) => {
+      console.error("Fehler beim Abrufen der Bücher:", error);
+      return [];
+    });
 }
-function createBookElement(title, authors, thumbnail, bookId, isSaved = false, mode = 'default') {
-    const bookItem = document.createElement('div');
-    bookItem.classList.add('book-item');
+function createBookElement(
+  title,
+  authors,
+  thumbnail,
+  bookId,
+  isSaved = false,
+  mode = "default"
+) {
+  const bookItem = document.createElement("div");
+  bookItem.classList.add("book-item");
 
-    if (mode === 'thumbnailOnly') {
-        // Nur das Thumbnail anzeigen
-        const bookCover = document.createElement('img');
-        if (thumbnail) {
-            bookCover.src = thumbnail;
-            bookCover.alt = `Cover von ${title}`;
-            bookItem.appendChild(bookCover);
-        }
-    } else {
-        // Vollständige Ansicht anzeigen
-        const bookTitle = document.createElement('h2');
-        bookTitle.textContent = title;
+  if (mode === "thumbnailOnly") {
+    // Nur das Thumbnail anzeigen
+    const bookCover = document.createElement("img");
+    if (thumbnail) {
+      bookCover.src = thumbnail;
+      bookCover.alt = `Cover von ${title}`;
+      bookItem.appendChild(bookCover);
+    }
+  } else {
+    // Vollständige Ansicht anzeigen
+    const bookTitle = document.createElement("h2");
+    bookTitle.textContent = title;
 
-        const bookAuthors = document.createElement('p');
-        bookAuthors.textContent = `Autor(en): ${authors}`;
+    const bookAuthors = document.createElement("p");
+    bookAuthors.textContent = `Autor(en): ${authors}`;
 
-        const bookCover = document.createElement('img');
-        if (thumbnail) {
-            bookCover.src = thumbnail;
-            bookCover.alt = `Cover von ${title}`;
-            bookItem.appendChild(bookCover);
-        }
-
-        bookItem.appendChild(bookTitle);
-        bookItem.appendChild(bookAuthors);
+    const bookCover = document.createElement("img");
+    if (thumbnail) {
+      bookCover.src = thumbnail;
+      bookCover.alt = `Cover von ${title}`;
+      bookItem.appendChild(bookCover);
     }
 
-    if (!isSaved) {
-        const saveButton = document.createElement('button');
-        saveButton.textContent = 'Gelesen';
-        saveButton.onclick = () => {
-            saveBookToLocalStorage({ title, authors, thumbnail, id: bookId });
-            showNotification('Buch hinzugefügt');
-        };
-        bookItem.appendChild(saveButton);
-    }
+    bookItem.appendChild(bookTitle);
+    bookItem.appendChild(bookAuthors);
+  }
 
-    if (isSaved) {
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Löschen';
-        deleteButton.onclick = () => {
-            deleteBookFromLocalStorage(bookId);
-            showNotification('Buch gelöscht');
-        };
-        bookItem.appendChild(deleteButton);
-    }
+  if (!isSaved) {
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Gelesen";
+    saveButton.onclick = () => {
+      saveBookToLocalStorage({ title, authors, thumbnail, id: bookId });
+      showNotification("Buch hinzugefügt");
+    };
+    bookItem.appendChild(saveButton);
+  }
 
-    return bookItem;
+  if (isSaved) {
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Löschen";
+    deleteButton.onclick = () => {
+      deleteBookFromLocalStorage(bookId);
+      showNotification("Buch gelöscht");
+    };
+    bookItem.appendChild(deleteButton);
+  }
+
+  return bookItem;
 }
 
 function addBookToContainer(containerId, bookElement) {
-    const container = document.getElementById(containerId);
-    if (container) {
-        container.appendChild(bookElement);
-    } else {
-        console.error('Container nicht gefunden:', containerId);
-    }
+  const container = document.getElementById(containerId);
+  if (container) {
+    container.appendChild(bookElement);
+  } else {
+    console.error("Container nicht gefunden:", containerId);
+  }
 }
 
 function deleteBookFromLocalStorage(bookId) {
-    localStorage.removeItem(`book_${bookId}`);
-    saveBooksToServer();
-    displaySavedBooks();
+  localStorage.removeItem(`book_${bookId}`);
+  saveBooksToServer();
+  displaySavedBooks();
 }
 
 function loadBooksFromLocalStorage() {
-    const savedBooks = [];
-    Object.keys(localStorage).forEach(key => {
-        if (key.startsWith('book_')) {
-            const book = JSON.parse(localStorage.getItem(key));
-            savedBooks.push(book);
-        }
-    });
-    return savedBooks;
+  const savedBooks = [];
+  Object.keys(localStorage).forEach((key) => {
+    if (key.startsWith("book_")) {
+      const book = JSON.parse(localStorage.getItem(key));
+      savedBooks.push(book);
+    }
+  });
+  return savedBooks;
 }
 
 function searchAndDisplayBooks() {
-    const query = document.getElementById('searchInput').value;
+  const query = document.getElementById("searchInput").value;
 
-    fetchBooks(query, apiKey).then(books => {
-        if (books && books.length > 0) {
-            displayBooks(books, 'book-list');
-        } else {
-            console.log('Keine Bücher gefunden');
-        }
-    });
+  fetchBooks(query, apiKey).then((books) => {
+    if (books && books.length > 0) {
+      displayBooks(books, "book-list");
+    } else {
+      console.log("Keine Bücher gefunden");
+    }
+  });
 }
 
 function saveBooksToServer() {
-    const savedBooks = loadBooksFromLocalStorage();
-    fetch('/save_books', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(savedBooks),
-    }).then(response => response.json())
-      .then(data => {
-          console.log(data.message);
-      })
-      .catch(error => {
-          console.error('Fehler beim Speichern auf dem Server:', error);
-      });
+  const savedBooks = loadBooksFromLocalStorage();
+  fetch("/save_books", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(savedBooks),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message);
+    })
+    .catch((error) => {
+      console.error("Fehler beim Speichern auf dem Server:", error);
+    });
 }
 
 async function loadBooksFromServer() {
-    try {
-        const response = await fetch('/load_books');
-        const books = await response.json();
-        books.forEach(book => {
-            localStorage.setItem(`book_${book.id}`, JSON.stringify(book));
-        });
-        displaySavedBooks();
-    } catch (error) {
-        console.error('Fehler beim Laden der Bücher:', error);
-    }
+  try {
+    const response = await fetch("/load_books");
+    const books = await response.json();
+    books.forEach((book) => {
+      localStorage.setItem(`book_${book.id}`, JSON.stringify(book));
+    });
+    displaySavedBooks();
+  } catch (error) {
+    console.error("Fehler beim Laden der Bücher:", error);
+  }
 }
 //ändern
 function getLastFiveBooks() {
-    const books = loadBooksFromLocalStorage();
-    
-    // Überprüfe die geladenen Bücher
-    console.log('Alle gespeicherten Bücher:', books);
-    
-    // Hole die letzten 5 Bücher oder weniger, wenn es weniger als 5 Bücher gibt
-    const lastFiveBooks = books.slice(-5);
-    
-    // Überprüfe die letzten 5 Bücher
-    console.log('Die letzten 5 Bücher:', lastFiveBooks);
-    
-    return lastFiveBooks;
+  const books = loadBooksFromLocalStorage();
+
+  // Überprüfe die geladenen Bücher
+  console.log("Alle gespeicherten Bücher:", books);
+
+  // Hole die letzten 5 Bücher oder weniger, wenn es weniger als 5 Bücher gibt
+  const lastFiveBooks = books.slice(-5);
+
+  // Überprüfe die letzten 5 Bücher
+  console.log("Die letzten 5 Bücher:", lastFiveBooks);
+
+  return lastFiveBooks;
 }
 
-
-//
 async function init() {
-    await  loadBooksFromServer(); // Lade Bücher von Server beim Start
-     displayLastFiveBooks(); // Zeige die letzten 5 Bücher an
+  await loadBooksFromServer(); // Lade Bücher von Server beim Start
+  displayLastFiveBooks(); // Zeige die letzten 5 Bücher an
 }
 
 function saveBookToLocalStorage(book) {
-    const bookWithDate = { ...book, addedDate: new Date().toISOString() };
-    const bookId = book.id;
-    localStorage.setItem(`book_${bookId}`, JSON.stringify(bookWithDate));
-    saveBooksToServer(); // Backup erstellen, wenn ein Buch gespeichert wird
-} 
+  const bookWithDate = { ...book, addedDate: new Date().toISOString() };
+  const bookId = book.id;
+  localStorage.setItem(`book_${bookId}`, JSON.stringify(bookWithDate));
+  saveBooksToServer(); // Backup erstellen, wenn ein Buch gespeichert wird
+}
 
 function displaySavedBooks() {
-    const container = document.getElementById('saved-books');
-    container.innerHTML = '';
+  const container = document.getElementById("saved-books");
+  container.innerHTML = "";
 
-    const savedBooks = loadBooksFromLocalStorage();
-    savedBooks.forEach(book => {
-        const bookElement = createBookElement(book.title, book.authors, book.thumbnail, book.id, true);
-        addBookToContainer('saved-books', bookElement);
-    });
+  const savedBooks = loadBooksFromLocalStorage();
+  savedBooks.forEach((book) => {
+    const bookElement = createBookElement(
+      book.title,
+      book.authors,
+      book.thumbnail,
+      book.id,
+      true
+    );
+    addBookToContainer("saved-books", bookElement);
+  });
 }
 
-// ändern?S
 function displayLastFiveBooks() {
-    const container = document.getElementById('latest-books-container');
-    if (!container) {
-        console.error('Container "latest-books-container" nicht gefunden');
-        return;
-    }
-    console.log('Container gefunden:', container);
+  const container = document.getElementById("latest-books-container");
+  if (!container) {
+    console.error('Container "latest-books-container" nicht gefunden');
+    return;
+  }
+  console.log("Container gefunden:", container);
 
-    const books = getLastFiveBooks();
-    console.log('Anzuzeigende Bücher:', books);
+  const books = getLastFiveBooks();
+  console.log("Anzuzeigende Bücher:", books);
 
-    displayBooks(books, 'latest-books-container', true, true, 'thumbnailOnly');
+  displayBooks(books, "latest-books-container", true, true, "thumbnailOnly");
 }
 
+function displayBooks(
+  books,
+  containerId,
+  clearContainer = false,
+  isSaved = false,
+  mode = "default"
+) {
+  const container = document.getElementById(containerId);
+  if (!container) {
+    console.error("Container nicht gefunden:", containerId);
+    return;
+  }
 
+  if (clearContainer) {
+    container.innerHTML = ""; // Container leeren, bevor neue Bücher hinzugefügt werden
+  }
 
-function displayBooks(books, containerId, clearContainer = false, isSaved = false, mode = 'default') {
-    const container = document.getElementById(containerId);
-    if (!container) {
-        console.error('Container nicht gefunden:', containerId);
-        return;
-    }
+  books.forEach((book) => {
+    console.log("Verarbeite Buch:", book); // Füge diese Zeile hinzu, um das Buchobjekt zu überprüfen
 
-    if (clearContainer) {
-        container.innerHTML = ''; // Container leeren, bevor neue Bücher hinzugefügt werden
-    }
+    const title = book.title || book.volumeInfo?.title || "Unbekannter Titel";
+    const authors = Array.isArray(book.authors)
+      ? book.authors.join(", ")
+      : book.authors || "Unbekannter Autor";
+    const thumbnail =
+      book.thumbnail || book.volumeInfo?.imageLinks?.thumbnail || "";
+    const bookId = book.id || book.volumeInfo?.id || "unknown_id";
 
-    books.forEach(book => {
-        console.log('Verarbeite Buch:', book); // Füge diese Zeile hinzu, um das Buchobjekt zu überprüfen
-
-        const title = book.title || book.volumeInfo?.title || 'Unbekannter Titel';
-        const authors = Array.isArray(book.authors) ? book.authors.join(', ') : book.authors || 'Unbekannter Autor';
-        const thumbnail = book.thumbnail || book.volumeInfo?.imageLinks?.thumbnail || '';
-        const bookId = book.id || book.volumeInfo?.id || 'unknown_id';
-
-        const bookElement = createBookElement(title, authors, thumbnail, bookId, isSaved, mode);
-        container.appendChild(bookElement);
-    });
+    const bookElement = createBookElement(
+      title,
+      authors,
+      thumbnail,
+      bookId,
+      isSaved,
+      mode
+    );
+    container.appendChild(bookElement);
+  });
 }
 
 function showNotification(message) {
-    const notification = document.getElementById('notification');
-    notification.textContent = message;
-    notification.style.display = 'block';
+  const notification = document.getElementById("notification");
+  notification.textContent = message;
+  notification.style.display = "block";
 
-    setTimeout(() => {
-        notification.style.display = 'none';
-    }, 3000);
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 3000);
 }
 
-window.onload = function() {
-    init(); // Stelle sicher, dass die init-Funktion aufgerufen wird, wenn du diese verwendest
+window.onload = function () {
+  init(); // Stelle sicher, dass die init-Funktion aufgerufen wird, wenn du diese verwendest
 
-    displayLastFiveBooks(); // Die letzten 5 Bücher beim Laden der Seite anzeigen
+  displayLastFiveBooks(); // Die letzten 5 Bücher beim Laden der Seite anzeigen
 };
 
-document.getElementById('searchButton').addEventListener('click', searchAndDisplayBooks);
+document
+  .getElementById("searchButton")
+  .addEventListener("click", searchAndDisplayBooks);
